b822917a7b72a7b19a6eecca22bcb680daf1e1de
AlfaCentaury <alfa.arnialfa@gmail.com>
2021-09-25 13:28:22 +0700

    Codelab 3

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -9,124 +9,131 @@ var informationTextStyle = TextStyle(
 class DetailScreen extends StatelessWidget {
   final TourismPlace place;
 
-  DetailScreen({@required this.place});
+  DetailScreen({required this.place});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.black,
-        body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Stack(
-                children: [
-                  Hero(
-                    tag: "imageBackground", 
-                    child: Image.asset(place.imageAsset)
-                  ),
-                  SafeArea(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Stack(
+              children: [
+                Hero(
+                  tag: "imageBackground", 
+                  child: Image.asset(place.imageAsset)
+                ),
+                SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        IconButton(
-                          icon: Icon(Icons.arrow_back), 
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
+                        CircleAvatar(
+                          backgroundColor: Colors.grey,
+                          child: IconButton(
+                            icon: Icon(
+                              Icons.arrow_back,
+                              color: Colors.white,
+                            ), 
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          ),
                         ),
-                        FavoriteButton(),
-                      ],
-                    )
+                      FavoriteButton(),
+                    ],
                   ),
-                ],
+                ),
               ),
-              Container(
-                  margin: EdgeInsets.only(top: 16.0),
-                  child: Text(
-                    place.name,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontSize: 30.0, 
-                        fontFamily: 'Staatliches',
-                        )
-                      )
-                    ),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 16.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            ],
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 16.0),
+            child: Text(
+              place.name,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30.0, 
+                fontFamily: 'Staatliches',
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
                   children: [
-                    Column(
-                      children: [
-                        Icon(Icons.calendar_today),
-                        SizedBox(height: 8.0),
-                        Text(
-                          place.openDays,
-                          style: informationTextStyle,
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Icon(Icons.access_time),
-                        SizedBox(height: 8.0),
-                        Text(
-                          place.openTime,
-                          style: informationTextStyle,
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Icon(Icons.monetization_on),
-                        SizedBox(height: 8.0),
-                        Text(
-                          place.ticketPrice,
-                          style: informationTextStyle,
-                        ),
-                      ],
+                    Icon(Icons.calendar_today),
+                    SizedBox(height: 8.0),
+                    Text(
+                      place.openDays,
+                      style: informationTextStyle,
                     ),
                   ],
                 ),
-              ),
-              Container(
-                padding: EdgeInsets.all(16.0),
-                child: Text(
-                  place.description,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontFamily: 'Oxygen',
-                  ),
+                Column(
+                  children: [
+                    Icon(Icons.access_time),
+                    SizedBox(height: 8.0),
+                    Text(
+                      place.openTime,
+                      style: informationTextStyle,
+                    ),
+                  ],
                 ),
-              ),
-              Container(
-                height: 150,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: place.imageUrls.map((url) {
-                    return Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: Image.network(url),
-                      ),
-                    );
-                  }).toList(),
+                Column(
+                  children: [
+                    Icon(Icons.monetization_on),
+                    SizedBox(height: 8.0),
+                    Text(
+                      place.ticketPrice,
+                      style: informationTextStyle,
+                    ),
+                  ],
                 ),
-              )
-            ],
+              ],
+            ),
           ),
-        )
-      );
+          Container(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              place.description,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16.0,
+                fontFamily: 'Oxygen',
+              ),
+            ),
+          ),
+          Container(
+              height: 150,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: place.imageUrls.map((url) {
+                  return Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(url),
+                    ),
+                  );
+                }).toList(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
 class FavoriteButton extends StatefulWidget {
   @override
   _FavoriteButtonState createState() => _FavoriteButtonState();
-
 }
 
 class _FavoriteButtonState extends State<FavoriteButton> {
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_bandung/main_screen.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Wisata Bandung',
-      theme: ThemeData.dark(),
+      theme: ThemeData(),
       home: MainScreen(),
     );
   }
